6a92dd44ab882dc2c828a9ca237966571bef2ab2
Ariq Athallah <ariqathallah@hotmail.com>
2021-02-08 12:18:23 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/create' exact component={Create} />
+            <Route path='/blogs/:id' component={BlogDetails} />
+            <Route path='*' component={NotFound} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
